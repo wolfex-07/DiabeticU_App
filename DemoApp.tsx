e91f45439ec2de98da50b50ec9433fb58333cbc6
Karan <karan.jaiswal@appinventiv.com>
2024-01-31 13:51:32 +0530

    project setup

---
+++ DemoApp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { LaunchScreen } from "./src/screens/launch";
+
+function DiabeticUApp() : React.JSX.Element {
+  return (
+    <LaunchScreen/>
+  )
+}
+
+export default DiabeticUApp;
---
+++ src/screens/launch/LaunchScreen.tsx
@@ -0,0 +1,18 @@
+// /src/screens/launch/SplashScreen.tsx
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+const LaunchScreen = () => {
+  useEffect(() => {
+     
+  }, []);
+
+  return (
+    <View>
+      <Text>Splash Screen</Text>
+      {/* Add any other components or styles for your splash screen */}
+    </View>
+  );
+};
+
+export default LaunchScreen;
